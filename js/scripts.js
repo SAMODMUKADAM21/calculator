1878db5de73a8c110c92b24bfeb667fe1bb20390
Mukadam Samod <samodmukadam2005@gmail.com>
2021-11-09 03:43:44 -0800

    Add functions to add

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a num"));
+const number2 = parseInt(prompt("Enter another num"));
+const result = add(number1, number2);
+alert(result);
+
+// Subtraction
+function subtract(num1,num2) {
+  return num1 - num2;
+}
+
+const number3 = parseInt(prompt("Enter a num"));
+const number4 = parseInt(prompt("Enter another num"));
+const subtractResult = subtract(number3, number4);
+alert(subtractResult);
